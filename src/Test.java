e874b2af33593b103467610e8f1c9d5b19a68e89
Chris Xu <xuchen@jiazhiyi.com>
2023-01-16 14:35:40 +0800

    Update Variable type content

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+  /* public static final  double CM_PER_INCH = 2.54;
+    public static void main(String[] args) {
+
+       double paperWidth = 8.5;
+       double paperHeight = 11;
+
+       System.out.println("Paper is in the centimeters: "+paperHeight*CM_PER_INCH+" by "+paperWidth*CM_PER_INCH);
+    }
+ enum Size {Small,Medium,Large,extraLarge}
+    Size s = Size.Medium;
+
+    public static void main(String[] args) {
+        System.out.println("Size s is ");
+    }*/
+
+}
