d8cc0190b54feda0f54d9b6aaaf74e66c9848987
Chris Xu <xuchen@jiazhiyi.com>
2023-01-17 14:53:03 +0800

    Update While loop Learning

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Test {
   /* public static final  double CM_PER_INCH = 2.54;
     public static void main(String[] args) {
@@ -14,4 +16,70 @@ public class Test {
         System.out.println("Size s is ");
     }*/
 
+    public static void main(String[] args) {
+        /* double x = 6;
+        double y = Math.sqrt(x);
+        double y = 7;
+        double x = Math.pow(3,y);
+        double x = Math.rint(8.5);
+        System.out.println(Math.ceil(Math.random()*100));
+
+        double x = 12.33313;
+        x+=4;
+        var y = (int) Math.ceil(x);
+        System.out.println(y);
+
+
+        String x = "Java\u2122";
+        System.out.println(x);
+        String x = "Hashimoto Nanami";
+        String expletive = "Expletive";
+        String PG13 = "deleted";
+        String message = expletive + PG13;
+        int age = 13;
+        String rating = "PG" + age;
+        String x = rating.repeat(3);
+
+        String greeting = "Hello";
+        greeting = greeting.substring(0, 3) + "p!";
+
+        String x = "Hashimoto";
+        String y = "橋本";
+        boolean z = x.equals(y);
+
+        String s = "2.0";
+        String t = "2";
+        System.out.println(t.repeat(3));
+
+       StringBuilder t = new StringBuilder("Hashimoto");
+
+       System.out.println(t.reverse());
+
+        String t = "Hashimoto\n Nanami\n 773";
+        String t = """
+                Hashimoto \
+                Nanami
+                773
+                """;
+                double x = 10000000000.0/3.0;
+        System.out.printf("%,.2f",x);
+        System.out.println(t);
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = in.nextLine();
+        System.out.println("How old are you? \n ");
+        int age = in.nextInt();
+        System.out.println("Hello, " + name + ". Next year,you'll be " + (age + 1));*/
+
+        int x = 100;
+        int y = 20;
+        if (x>y) {
+        System.out.println("Ture");}
+        else System.out.println("False");
+
+
+
+
+
+    }
 }
